0ca9670397945b8189dc6c4780d2fb58713e4a2f
CristóbalO_Kronos <dondoncronos@gmail.com>
2022-10-04 12:24:49 -0300

    sqlite + database

--- app/src/main/java/com/example/prueba01dsm/SecondFragment.java
+++ app/src/main/java/com/example/prueba01dsm/SecondFragment.java
@@ -1,11 +1,19 @@
 package com.example.prueba01dsm;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -15,11 +23,15 @@ public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
     private String mail, user, url;
+    private WebView wv1;
+    SharedPreferences pref = getActivity().getSharedPreferences("datos", Context.MODE_PRIVATE);
+    String email = pref.getString("email", "empty");
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
+
     ) {
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
@@ -41,8 +53,12 @@ public class SecondFragment extends Fragment {
             //Guardar
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                setContentView(R.layout.activity_web);
+                wv1= (WebView) findViewById(R.id.wv1);
+                String url=getIntent().getStringExtra("url");
+                wv1.setWebViewClient(new WebViewClient());
+                Intent i= new Intent(this.getContext(), ActivityWeb.class);
+                i.putExtra("url",txt_search.getText().toString());
             }
         });
         binding.button3.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +85,24 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+    }
+
+    public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+        public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+            //VAMOS A CREAR LA TABLA
+            sqLiteDatabase.execSQL("create table products(code int primary key, description text, price real)");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        }
     }
 
     @Override
